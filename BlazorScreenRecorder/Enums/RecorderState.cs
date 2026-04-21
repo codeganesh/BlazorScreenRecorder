24d316b7646914b6be9aaa04a43b12ea1b021475
codeg <codeg@CODEGANESH>
2026-04-21 13:49:15 +0300

    Add project files.

---
+++ BlazorScreenRecorder/Enums/RecorderState.cs
@@ -0,0 +1,7 @@
+﻿namespace BlazorScreenRecorder.Enums
+{
+    public enum RecorderState
+    {
+        Idle,NotRecording,Recording,RecordingComplete
+    }
+}
---
+++ BlazorScreenRecorder/Shared/ScreenRecorder.razor.js
@@ -0,0 +1,79 @@
+﻿export async function addScreenToVideo(videoElement, screenOptions, dotNetHelper) {
+
+    try {
+
+        videoElement.dotNetHelper = dotNetHelper;
+
+
+        var screen = await navigator.mediaDevices.getDisplayMedia(screenOptions);
+
+
+
+        screen.getTracks()[0].onended = async () => {
+
+            videoElement.srcObject = null;
+            videoElement.muted = true;
+
+            await videoElement.dotNetHelper.invokeMethodAsync("ScreenEnded");
+        }
+
+        videoElement.srcObject = screen;
+        videoElement.muted = true;
+
+
+
+    }
+    catch (err) {
+        throw err.message;
+    }
+}
+
+export function getRecorder(videoElement) {
+    var chunks = [];
+
+    var screen = videoElement.srcObject;
+
+    var options = null;
+
+
+    if (MediaRecorder.isTypeSupported("video/mp4")) {
+
+        options = {
+            mime: "video/mp4",
+            ext: "mp4"
+        }
+    }
+    else {
+        options = {
+            mime: "video/webm",
+            ext: "webm"
+        }
+    }
+
+    var recorder = new MediaRecorder(screen, { mimeType: options.mime });
+    recorder.ondataavailable = function onDataAvailable(e) {
+
+        if (e.data.size > 0) {
+            chunks.push(e.data);
+        }
+    }
+    recorder.onstop = async function onStop() {
+
+        var blob = new Blob(chunks, { type: recorder.mimeType });
+        var mediaBlobUrl = URL.createObjectURL(blob);
+
+        
+        screen.getTracks().forEach(track => {
+            track.stop();
+        });
+
+        videoElement.srcObject = null;
+
+        await videoElement.dotNetHelper.invokeMethodAsync("RecordComplete", mediaBlobUrl,options.ext);
+    }
+
+    return recorder;
+
+
+
+}
